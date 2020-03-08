aeac2e4c4d0454c8ed5b47b0092f5306fa662b2f
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-03-08 14:04:19 +0100

    Construction du projet maven et ajout de la classe  RobotStatic

---
+++ pglp3_3/src/main/java/pglp3/pglp3_3/RobotStatic.java
@@ -0,0 +1,26 @@
+package pglp3.pglp3_3;
+
+public class RobotStatic {
+private int position ;
+	
+
+	
+	
+	public RobotStatic(int position) {
+		this.position=position;
+	}
+		
+	
+	
+	public void avance() {
+		
+		throw new UnsupportedOperationException();
+		
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	
+}
