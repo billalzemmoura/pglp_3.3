e8d9cdcb4bc037243f8733ebacc569ea0c7df849
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-03-08 13:33:51 +0100

    Construction du projet maven et ajout de la classe  Robot

---
+++ pglp3_3/src/main/java/pglp3/pglp3_3/Robot.java
@@ -0,0 +1,15 @@
+package pglp3.pglp3_3;
+
+public class Robot {
+	protected int  position;
+	
+	 public Robot(int position) {
+		this.position=position;	
+		}
+	 
+	 public void avance() {
+			
+			position+=5;
+			
+		}
+}
