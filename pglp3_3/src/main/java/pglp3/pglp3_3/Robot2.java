50e0d6e18de049359a297bcb37dcf5450939bb18
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-03-08 14:09:07 +0100

    Construction du projet maven et ajout de la classe  Robot2 qui avance de 15 unité

---
+++ pglp3_3/src/main/java/pglp3/pglp3_3/Robot2.java
@@ -0,0 +1,15 @@
+package pglp3.pglp3_3;
+
+public class Robot2 extends Robot {
+
+	public Robot2(int position) {
+		super(position);
+		
+	}
+@Override
+public void avance() {
+	
+	super.position+=15;
+	
+}
+}
